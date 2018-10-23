499f44a70ca4103b133ec05425c5cc9bca5660c8
Bhushan Rane <bhushan.rane@capgemini.com>
2018-10-23 15:45:26 +0530

    ds

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,9 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-func new1()
-{
-    
+    func new1()
+    {
+        print("hello")
     }
 
 }
