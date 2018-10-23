ccc660a5bd62afbb193ee50f59acd27534765a8a
Bhushan Rane <bhushan.rane@capgemini.com>
2018-10-23 15:48:39 +0530

    fg

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -13,11 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("new2")
     }
-    func new1()
-    {
-        print("hello")
-    }
-
 }
 
